32c6705c95809e42f8651a00c6fba0020cc8415e
Donovan <SimonDon@berkeleyprep.org>
2019-01-28 22:52:03 -0500

    qwertyuiop

--- Homework12.java
+++ Homework12.java
@@ -12,13 +12,13 @@ public class Homework12 {
 	 * ArrayList of RectangularShape values
 	 */
 	public Homework12() {
-
+			arr = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+			arr.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +26,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+			int num = 0;
+			for(int i = 0; i < arr.size(); i++) {
+				if (arr.get(i).intersects(r)) {
+					num++;
+				}
+			}
+			return num;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,21 +40,40 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int num = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (!arr.get(i).contains(p)) {
+				num++;
+			}
+		}
+		return num;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int total = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) instanceof Ellipse2D) {
+				total++;
+			}
+		}
+		return total;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		for (RectangularShape shape : arr) {
+			if (shape instanceof Rectangle2D) {
+				if (l.intersects((Rectangle2D) shape)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +85,15 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double area = 0.0;
+		for (RectangularShape shape : arr) {
+			if (shape instanceof Rectangle2D) {
+				area += shape.getHeight() * shape.getWidth();
+			} else if (shape instanceof Ellipse2D) {
+				area += Math.PI * (shape.getWidth() / 2) * (shape.getHeight() / 2);
+			}
+		}
+		return area;
 	}
 
 	public static void main(String[] args) {
